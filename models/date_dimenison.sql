247c5cd3571929cffa4845095c0d02da91812216
knowhownath@gmail.com <knowhownath@gmail.com>
2025-10-03 10:01:40 +0000

    Station_dimension created

--- models/date_dimenison.sql
+++ models/date_dimenison.sql
@@ -4,11 +4,8 @@ select
 to_timestamp(STARTED_AT) AS STARTED_AT
 ,date(to_timestamp(STARTED_AT)) AS DATE_STARTED_AT
 ,hour(to_timestamp(STARTED_AT)) AS HOUR_STARTED_AT
-,CASE WHEN DAYNAME(to_timestamp(STARTED_AT)) IN ('sat','sun')
-then 'WEEKEND' ELSE 'BUSINESSDAY' END AS DAY_TYPE,
-CASE WHEN MONTH(to_timestamp(STARTED_AT)) IN (12,1,2) THEN 'WINTER'
-WHEN MONTH(to_timestamp(STARTED_AT)) IN (3,4,5) THEN 'SPRING'
-WHEN MONTH(to_timestamp(STARTED_AT)) IN (6,7,8) THEN 'SUMMER' ELSE 'AUTUMN' END AS STATION_OF_YEAR
+,{{DAY_TYPE('STARTED_AT')}} AS DAY_TYPE
+,{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 from 
 {{ source('DBT_SRC_DEMO', 'bike') }}
 where STARTED_AT <>'started_at'
